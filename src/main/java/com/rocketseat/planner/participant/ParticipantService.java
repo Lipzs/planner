27b3480a081b686e9bda2cd0fe02edbec3a1390c
Lipzs <filipedias823@hotmail.com>
2024-07-10 21:56:48 -0300

    feat: implementação das rotas de criar e consultar detalhes da viagem

---
+++ src/main/java/com/rocketseat/planner/participant/ParticipantService.java
@@ -0,0 +1,12 @@
+package com.rocketseat.planner.participant;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParticipantService {
+  public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId) {}
+  public void triggerConfirmationEmailToParticipants(UUID tripId) {}
+}
